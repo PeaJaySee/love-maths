92d3c61f7178198c3f91ba14a67429ea401e1986
Phil <phil.jim.cooper@gmail.com>
2025-02-07 23:05:36 +0000

    score incrementation added

--- assets/js/script.js
+++ assets/js/script.js
@@ -53,10 +53,12 @@ function checkAnswer() {
 
     if (isCorrect) {
         alert("Hey! You got it right! :D");
+        incrementScore();
     }
 
     else {
         alert(`Ah, you answered ${userAnswer}, the correct answer was ${calculatedAnswer[0]}!`);
+        incrementWrongAnswer();
     }
 
     runGame(calculatedAnswer[1]);
@@ -83,12 +85,22 @@ function calculateCorrectAnswer() {
 
 }
 
-function incrementScore() {
+/**
+ * Gets the current score from the DOM and increments it by 1
+ */
 
+function incrementScore() {
+    let oldScore = parseInt(document.getElementById("score").innerText);
+    document.getElementById("score").innerText = ++oldScore;
 }
 
-function incrementWrongAnswer() {
+/**
+ * Gets the current incorrect tally from the DOM and increments it by 1
+ */
 
+function incrementWrongAnswer() {
+    let oldTally = parseInt(document.getElementById("incorrect").innerText);
+    document.getElementById("incorrect").innerText = ++oldTally;
 }
 
 function displayAdditionQuestion(operand1, operand2) {
